acfd1a7f9bb971fa86be5d13e8027c821c47d439
Martin <anonymized_martin@schnst.at>
2024-05-11 11:01:05 +0200

    feat(55): Added manage user component to user home component.

--- frontend/src/app/components/user/manage-user/manage-user.component.ts
+++ frontend/src/app/components/user/manage-user/manage-user.component.ts
@@ -1,12 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-manage-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './manage-user.component.html',
-  styleUrl: './manage-user.component.scss'
+  selector: "app-manage-user",
+  templateUrl: "./manage-user.component.html",
+  styleUrl: "./manage-user.component.scss",
 })
-export class ManageUserComponent {
-
-}
+export class ManageUserComponent {}
--- frontend/src/app/components/user/user-home/user-home.component.ts
+++ frontend/src/app/components/user/user-home/user-home.component.ts
@@ -1,8 +1,36 @@
 import { Component } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+
+export enum Role {
+  admin = "ADMIN",
+  user = "USER",
+}
 
 @Component({
   selector: "app-user-home",
   templateUrl: "./user-home.component.html",
   styleUrl: "./user-home.component.scss",
 })
-export class UserHomeComponent {}
+export class UserHomeComponent {
+  isAdmin: boolean = false;
+  showManageUserForm: boolean = true;
+  constructor(private authService: AuthService) {}
+
+  selectedLinkId = "manageUser"; // Initial active link ID
+
+  toggleRegistration(id: string) {
+    this.selectedLinkId = id;
+    this.showManageUserForm = false;
+    if (id === "manageUser") {
+      this.showManageUserForm = true;
+    }
+  }
+
+  ngOnInit() {
+    this.isAdmin = this.authService.getUserRole() === Role.admin;
+  }
+
+  public get heading(): string {
+    return this.isAdmin ? "Admin Home Page" : "User Home Page";
+  }
+}
