e29d46d260e1f54302129b8a9fdebc94291c46b8
Martin <anonymized_martin@schnst.at>
2024-05-11 17:33:41 +0200

    feat(55): Created manage user component. Added function for loading data from backend.

--- frontend/src/app/components/user/manage-user/manage-user.component.ts
+++ frontend/src/app/components/user/manage-user/manage-user.component.ts
@@ -1,8 +1,41 @@
 import { Component } from "@angular/core";
+import { EmailValidator } from "@angular/forms";
+import { UserSearch } from "src/app/dtos/user-search";
+import { MessagingService } from "src/app/services/messaging.service";
+import {
+  ApplicationUserResponse,
+  UserEndpointService,
+} from "src/app/services/openapi";
 
 @Component({
   selector: "app-manage-user",
   templateUrl: "./manage-user.component.html",
   styleUrl: "./manage-user.component.scss",
 })
-export class ManageUserComponent {}
+export class ManageUserComponent {
+  searchParams: UserSearch = {
+    email: "",
+  };
+  user: ApplicationUserResponse = null;
+
+  constructor(
+    private userEndpointService: UserEndpointService,
+    private messagingService: MessagingService
+  ) {}
+
+  searchUser() {
+    this.userEndpointService
+      .findUserByEmail(this.searchParams.email)
+      .subscribe({
+        next: (user) => {
+          // Cast to ApplicationUserDto
+          console.log("User loaded successfully:", user);
+          this.user = user;
+          this.messagingService.setMessage("User loaded successfully.");
+        },
+        error: (error) => console.error("Error loading user:", error),
+      });
+  }
+
+  updateLockStatus(status: boolean) {}
+}
