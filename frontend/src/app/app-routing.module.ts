b21d9b99b22264e5f7f320396dba3a35e5dbb5b3
Martin <anonymized_martin@schnst.at>
2024-05-11 10:35:39 +0200

    feat(55): Created user manage and home component. Added route for user home component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
-import {NgModule} from '@angular/core';
-import {mapToCanActivate, RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
-import {LoginComponent} from './components/login/login.component';
-import {AuthGuard} from './guards/auth.guard';
-import {MessageComponent} from './components/message/message.component';
-import {RegistrationComponent} from "./components/user/registration/registration.component";
+import { NgModule } from "@angular/core";
+import { mapToCanActivate, RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { MessageComponent } from "./components/message/message.component";
+import { RegistrationComponent } from "./components/user/registration/registration.component";
+import { UserHomeComponent } from "./components/user/user-home/user-home.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'register', component: RegistrationComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'message', canActivate: mapToCanActivate([AuthGuard]), component: MessageComponent}
+  { path: "", component: HomeComponent },
+  { path: "register", component: RegistrationComponent },
+  { path: "login", component: LoginComponent },
+  {
+    path: "message",
+    canActivate: mapToCanActivate([AuthGuard]),
+    component: MessageComponent,
+  },
+  { path: "user/home", component: UserHomeComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './components/header/header.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {HomeComponent} from './components/home/home.component';
-import {LoginComponent} from './components/login/login.component';
-import {MessageComponent} from './components/message/message.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {httpInterceptorProviders} from './interceptors';
-import {RegistrationComponent} from "./components/user/registration/registration.component";
-import {GlobalMessageComponent} from "./global-message/global-message.component";
-import {AngularPhoneNumberInput} from "angular-phone-number-input";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { MessageComponent } from "./components/message/message.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { httpInterceptorProviders } from "./interceptors";
+import { RegistrationComponent } from "./components/user/registration/registration.component";
+import { GlobalMessageComponent } from "./global-message/global-message.component";
+import { AngularPhoneNumberInput } from "angular-phone-number-input";
+import { UserHomeComponent } from "./components/user/user-home/user-home.component";
+import { ManageUserComponent } from "./components/user/manage-user/manage-user.component";
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import {AngularPhoneNumberInput} from "angular-phone-number-input";
     LoginComponent,
     MessageComponent,
     RegistrationComponent,
-    GlobalMessageComponent
+    GlobalMessageComponent,
+    UserHomeComponent,
+    ManageUserComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,14 +38,10 @@ import {AngularPhoneNumberInput} from "angular-phone-number-input";
     HttpClientModule,
     NgbModule,
     FormsModule,
-    AngularPhoneNumberInput
+    AngularPhoneNumberInput,
   ],
   providers: [httpInterceptorProviders],
-  exports: [
-    HeaderComponent,
-    FooterComponent
-  ],
-  bootstrap: [AppComponent]
+  exports: [HeaderComponent, FooterComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
