3668b34baaacf12c0e86abfecf585d721e130f2d
Martin <anonymized_martin@schnst.at>
2024-05-17 17:53:39 +0200

    fix(): Customer was admin. Is now set to customer type.

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/UserDataGenerator.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/UserDataGenerator.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.sepr.groupphase.backend.datagenerator;
 
 import at.ac.tuwien.sepr.groupphase.backend.persistence.entity.Admin;
+import at.ac.tuwien.sepr.groupphase.backend.persistence.entity.Customer;
 import at.ac.tuwien.sepr.groupphase.backend.persistence.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +13,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDataGenerator {
 
+    private static final Admin ADMIN_USER = new Admin("admin@email.com", "", "Bert", "Berta", "+430987654321", "abcdefghijklmnopqrstuvwxyz", 0, false);
+    private static final Customer CUSTOMER_USER = new Customer("user@email.com", "", "Erna", "Erna", "+431234567890", "zyxwvutsrqponmlkjihgfedcba", 0, false);
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @PostConstruct
     private void generateData() {
-        ADMIN_USER.setPassword(passwordEncoder.encode("password"+ADMIN_USER.getSalt()));
-        CUSTOMER_USER.setPassword(passwordEncoder.encode("password"+CUSTOMER_USER.getSalt()));
+        ADMIN_USER.setPassword(passwordEncoder.encode("password" + ADMIN_USER.getSalt()));
+        CUSTOMER_USER.setPassword(passwordEncoder.encode("password" + CUSTOMER_USER.getSalt()));
         userRepository.save(ADMIN_USER);
         userRepository.save(CUSTOMER_USER);
     }
-
-    private static final Admin ADMIN_USER = new Admin("admin@email.com", "", "Bert", "Berta", "+430987654321", "abcdefghijklmnopqrstuvwxyz", 0, false);
-    private static final Admin CUSTOMER_USER = new Admin("user@email.com", "", "Erna", "Erna", "+431234567890", "zyxwvutsrqponmlkjihgfedcba", 0, false);
 }
