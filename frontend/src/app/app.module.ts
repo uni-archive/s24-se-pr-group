2094db36f5e9444bce961e9e21c8c46fb9e154ba
Martin <anonymized_martin@schnst.at>
2024-05-17 17:12:26 +0200

    fix(): Updated imports.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,20 +3,22 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './components/header/header.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {HomeComponent} from './components/home/home.component';
-import {LoginComponent} from './components/login/login.component';
-import {MessageComponent} from './components/message/message.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {httpInterceptorProviders} from './interceptors';
-import {RegistrationComponent} from "./components/user/registration/registration.component";
-import {GlobalMessageComponent} from "./global-message/global-message.component";
-import {AngularPhoneNumberInput} from "angular-phone-number-input";
-import {PrintPurchaseInvoiceButtonComponent} from "./components/print-purchase-invoice-button/print-purchase-invoice-button.component";
-import {PrintTicketButtonComponent} from "./components/print-ticket-button/print-ticket-button.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { MessageComponent } from "./components/message/message.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { httpInterceptorProviders } from "./interceptors";
+import { RegistrationComponent } from "./components/user/registration/registration.component";
+import { GlobalMessageComponent } from "./global-message/global-message.component";
+import { AngularPhoneNumberInput } from "angular-phone-number-input";
+import { PrintPurchaseInvoiceButtonComponent } from "./components/print-purchase-invoice-button/print-purchase-invoice-button.component";
+import { PrintTicketButtonComponent } from "./components/print-ticket-button/print-ticket-button.component";
+import { UserHomeComponent } from "./components/user/user-home/user-home.component";
+import { ManageUserComponent } from "./components/user/manage-user/manage-user.component";
 
 @NgModule({
   declarations: [
@@ -31,17 +33,17 @@ import {PrintTicketButtonComponent} from "./components/print-ticket-button/print
     UserHomeComponent,
     ManageUserComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        NgbModule,
-        FormsModule,
-        AngularPhoneNumberInput,
-        PrintPurchaseInvoiceButtonComponent,
-        PrintTicketButtonComponent
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    AngularPhoneNumberInput,
+    PrintPurchaseInvoiceButtonComponent,
+    PrintTicketButtonComponent,
+  ],
   providers: [httpInterceptorProviders],
   exports: [HeaderComponent, FooterComponent],
   bootstrap: [AppComponent],
