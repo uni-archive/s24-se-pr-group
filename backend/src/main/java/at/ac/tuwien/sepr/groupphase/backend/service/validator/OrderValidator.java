19449dd815b796af25e208afb285a4b42133626d
Benjamin <anonymized_benjamin@schnst.at>
2024-05-30 00:10:56 +0200

    feat(49): Added cancellation period to order.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/validator/OrderValidator.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/validator/OrderValidator.java
@@ -6,11 +6,18 @@ import at.ac.tuwien.sepr.groupphase.backend.persistence.entity.type.InvoiceType;
 import at.ac.tuwien.sepr.groupphase.backend.service.exception.ValidationException;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalUnit;
+import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class OrderValidator extends AbstractValidator<OrderDetailsDto> {
+
+    private static final TemporalAmount ORDER_CANCELLATION_PERIOD = Duration.ofDays(14);
 
     public void validateForFindById(OrderDetailsDto foundOrder, ApplicationUserDto user) throws ValidationException {
         List<String> errors = new ArrayList<>();
@@ -31,6 +38,11 @@ public class OrderValidator extends AbstractValidator<OrderDetailsDto> {
 
         if (order.getInvoices().stream().anyMatch(i -> i.getInvoiceType().equals(InvoiceType.CANCELLATION))) {
             errors.add("Order already cancelled.");
+        }
+
+        var now = LocalDateTime.now();
+        if (order.getDateTime().plus(ORDER_CANCELLATION_PERIOD).isBefore(now)) {
+            errors.add("Order exceeded cancellation period.");
         }
 
         endValidation(errors);
