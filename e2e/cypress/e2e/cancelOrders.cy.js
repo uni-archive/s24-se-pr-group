2718c4ea2c42be2304313a3230cd607b7d08e5d4
Benjamin <anonymized_benjamin@schnst.at>
2024-06-03 03:03:02 +0200

    feat(49): Added e2e tests.

---
+++ e2e/cypress/e2e/cancelOrders.cy.js
@@ -0,0 +1,59 @@
+context("cancel orders", () => {
+    beforeEach(() => {
+        cy.loginUser("cancelorder-user@email.com", "password");
+    })
+
+    describe("cancelling a valid empty order", () => {
+        it("marks the order as cancelled", () => {
+            const orderId = 1;
+            cy.cancelOrder(orderId);
+            cy.contains("Ihre Bestellung wurde erfolgreich storniert.");
+            cy.contains("Storno-Rechnung drucken");
+            cy.visit("/#/my/orders");
+            cy.get(`th[data-testid="order-id-${orderId}"]`)
+                .parent()
+                .contains("Ja");
+        });
+    });
+
+    describe("cancelling a valid non-empty order", () => {
+        it("marks the order as cancelled", () => {
+            const orderId = 2;
+            cy.cancelOrder(orderId);
+            cy.contains("Ihre Bestellung wurde erfolgreich storniert.");
+            cy.contains("Storno-Rechnung drucken");
+            cy.visit("/#/my/orders");
+            cy.get(`th[data-testid="order-id-${orderId}"]`)
+                .parent()
+                .contains("Ja");
+        });
+    });
+
+    describe("cancelling an order with expired cancellation-period", () => {
+        it("does not mark it as cancelled and shows error", () => {
+            const orderId = 3;
+            cy.cancelOrder(orderId);
+            cy.contains("Die Bestellung darf nicht storniert werden");
+            cy.contains("Storno-Rechnung drucken").should('not.exist');
+            cy.get(":visible").contains("Bestellung stornieren");
+            cy.visit("/#/my/orders");
+            cy.get(`th[data-testid="order-id-${orderId}"]`)
+                .parent()
+                .contains("Nein");
+        });
+    });
+
+    describe("cancelling an order that includes a show that already started", () => {
+        it("does not mark it as cancelled and shows error", () => {
+            const orderId = 5;
+            cy.cancelOrder(orderId);
+            cy.contains("Die Bestellung darf nicht storniert werden");
+            cy.contains("Storno-Rechnung drucken").should('not.exist');
+            cy.get(":visible").contains("Bestellung stornieren");
+            cy.visit("/#/my/orders");
+            cy.get(`th[data-testid="order-id-${orderId}"]`)
+                .parent()
+                .contains("Nein");
+        });
+    });
+})
