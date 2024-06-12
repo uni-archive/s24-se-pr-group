01bb83dcbf7dd8ca839192adc8064b6eae295d1d
Sebastian <anonymized_sebastian@schnst.at>
2024-06-12 16:56:02 +0200

    fix(26): Fixed mapping cycle issue.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/persistence/dao/NewsDao.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/persistence/dao/NewsDao.java
@@ -5,6 +5,7 @@ import at.ac.tuwien.sepr.groupphase.backend.dto.NewsDto;
 import at.ac.tuwien.sepr.groupphase.backend.persistence.entity.ApplicationUser;
 import at.ac.tuwien.sepr.groupphase.backend.persistence.entity.News;
 import at.ac.tuwien.sepr.groupphase.backend.persistence.exception.EntityNotFoundException;
+import at.ac.tuwien.sepr.groupphase.backend.persistence.mapper.CycleAvoidingMappingContext;
 import at.ac.tuwien.sepr.groupphase.backend.persistence.mapper.NewsMapper;
 import at.ac.tuwien.sepr.groupphase.backend.persistence.repository.NewsRepository;
 import at.ac.tuwien.sepr.groupphase.backend.persistence.repository.UserRepository;
@@ -12,28 +13,36 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class NewsDao extends AbstractDao<News, NewsDto> {
     private final NewsRepository newsRepository;
     private final UserRepository userRepository;
+    private final NewsMapper mapper;
+
+
+    @Override
+    public NewsDto findById(Long id) throws EntityNotFoundException {
+        return mapper.toDtoWithContext(newsRepository.findById(id).orElseThrow(()
+            -> new EntityNotFoundException(id)), new CycleAvoidingMappingContext());
+    }
 
     public NewsDao(NewsRepository repository, NewsMapper mapper, UserRepository userRepository) {
         super(repository, mapper);
         this.newsRepository = repository;
         this.userRepository = userRepository;
+        this.mapper = mapper;
     }
 
     public Page<NewsDto> findAllByOrderByPublishedAtDesc(Pageable pageable) {
         return newsRepository.findAllByOrderByPublishedAtDesc(pageable)
-            .map(mapper::toDto);
+            .map(news -> mapper.toDtoWithContext(news, new CycleAvoidingMappingContext()));
     }
 
     public void updateNewsWithUsers(NewsDto newsDto) throws EntityNotFoundException {
         if (!repository.existsById(newsDto.getId())) {
             throw new EntityNotFoundException(newsDto.getId());
         }
-        News news = mapper.toEntity(newsDto);
+        News news = mapper.toEntityWithContext(newsDto, new CycleAvoidingMappingContext());
         for (ApplicationUserDto userDto : newsDto.getUsers()) {
             ApplicationUser user = userRepository.findByEmail(userDto.getEmail());
             if (!user.getNews().contains(news)) {
@@ -49,8 +58,6 @@ public class NewsDao extends AbstractDao<News, NewsDto> {
             throw new EntityNotFoundException(userId);
         }
         return newsRepository.findUnseenNewsByUser(userId, pageable)
-            .map(mapper::toDto);
+            .map(news -> mapper.toDtoWithContext(news, new CycleAvoidingMappingContext()));
     }
-    
 }
-
