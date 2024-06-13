5aeb427f61b6b1d4173b8655b669e09a3a2ade9e
Sebastian <anonymized_sebastian@schnst.at>
2024-06-13 17:36:19 +0200

    feat(33): Start page shows scrollable news previews.

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from '../../services/auth.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { SimpleNewsResponseDto, NewsEndpointService } from '../../services/openapi';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,48 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  news: SimpleNewsResponseDto[] = [];
+  itemsPerPage: number = 3;
 
-  constructor(public authService: AuthService) { }
+  @ViewChild('scrollContainer', { static: false }) scrollContainer: ElementRef;
+
+  constructor(
+      public authService: AuthService,
+      private newsService: NewsEndpointService
+  ) {}
 
   ngOnInit() {
+    this.loadNews();
   }
 
+  loadNews() {
+    this.newsService.findUnread(0, 9).subscribe({
+      next: (response: any) => {
+        this.news = response.content;
+      },
+      error: error => {
+        console.error('Error loading news', error);
+      }
+    });
+  }
+
+  nextNews() {
+    this.scroll('next');
+  }
+
+  prevNews() {
+    this.scroll('prev');
+  }
+
+  scroll(direction: 'next' | 'prev') {
+    const container = this.scrollContainer.nativeElement;
+    const cardWidth = container.querySelector('.col-4').clientWidth + 30;
+    const scrollAmount = this.itemsPerPage * (cardWidth - 30);
+
+    if (direction === 'next') {
+      container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    } else {
+      container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    }
+  }
 }
