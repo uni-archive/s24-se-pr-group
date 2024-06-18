4f1d48ae69d9a6b1a5b273abb2ff4b74a3f1566d
Martin <anonymized_martin@schnst.at>
2024-06-18 07:54:00 +0200

    feat(54): Updated openapi files.

--- frontend/src/app/services/openapi/model/page-location-dto.ts
+++ frontend/src/app/services/openapi/model/page-location-dto.ts
@@ -15,16 +15,16 @@ import { LocationDto } from './location-dto';
 
 
 export interface PageLocationDto { 
-    totalPages?: number;
     totalElements?: number;
-    pageable?: PageableObject;
+    totalPages?: number;
+    first?: boolean;
+    last?: boolean;
     size?: number;
     content?: Array<LocationDto>;
     number?: number;
     sort?: Array<SortObject>;
     numberOfElements?: number;
-    first?: boolean;
-    last?: boolean;
+    pageable?: PageableObject;
     empty?: boolean;
 }
 
--- frontend/src/app/services/openapi/model/page-show-response.ts
+++ frontend/src/app/services/openapi/model/page-show-response.ts
@@ -15,16 +15,16 @@ import { SortObject } from './sort-object';
 
 
 export interface PageShowResponse { 
-    totalPages?: number;
     totalElements?: number;
-    pageable?: PageableObject;
+    totalPages?: number;
+    first?: boolean;
+    last?: boolean;
     size?: number;
     content?: Array<ShowResponse>;
     number?: number;
     sort?: Array<SortObject>;
     numberOfElements?: number;
-    first?: boolean;
-    last?: boolean;
+    pageable?: PageableObject;
     empty?: boolean;
 }
 
