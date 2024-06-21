a65713bf99fd44f7f3a061b6d84e269d9c39b846
Martin <anonymized_martin@schnst.at>
2024-06-21 18:27:36 +0200

    fix(): Updated user update request record.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/UserUpdateInfoRequest.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/UserUpdateInfoRequest.java
@@ -1,4 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
-public record UserUpdateInfoRequest(long id, String email, String phoneNumber) {
+import at.ac.tuwien.sepr.groupphase.backend.dto.AddressDto;
+
+public record UserUpdateInfoRequest(long id, String email, String phoneNumber, AddressDto address) {
 }
