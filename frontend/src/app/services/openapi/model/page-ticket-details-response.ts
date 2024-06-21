9cf62965104211c9b44df10c2d8bf1433b5b094d
Martin <anonymized_martin@schnst.at>
2024-06-21 12:50:44 +0200

    fix(): Updated openapi files.

--- frontend/src/app/services/openapi/model/page-ticket-details-response.ts
+++ frontend/src/app/services/openapi/model/page-ticket-details-response.ts
@@ -15,16 +15,16 @@ import { TicketDetailsResponse } from './ticket-details-response';
 
 
 export interface PageTicketDetailsResponse { 
-    totalElements?: number;
     totalPages?: number;
+    totalElements?: number;
+    first?: boolean;
+    last?: boolean;
     size?: number;
     content?: Array<TicketDetailsResponse>;
     number?: number;
     sort?: Array<SortObject>;
-    numberOfElements?: number;
-    first?: boolean;
-    last?: boolean;
     pageable?: PageableObject;
+    numberOfElements?: number;
     empty?: boolean;
 }
 
